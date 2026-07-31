8df7f2ffd0675b0b9f84dc54bcab232dde6f92c2
Fyre Mael <fyremaelstrom@gmail.com>
2026-07-30 19:30:58 -0700

    Export bounded training pipeline

--- tensorgraph/pipeline/__init__.py
+++ tensorgraph/pipeline/__init__.py
@@ -1,5 +1,13 @@
 """Bounded, evidence-producing compiler pipelines."""
 
+from .training_elementwise import (
+    CompiledElementwiseTraining,
+    GeneratedElementwiseBackwardKernel,
+    GeneratedElementwiseTraining,
+    compile_fx_elementwise_training,
+    load_generated_backward_kernel,
+    load_generated_training,
+)
 from .verified_elementwise import (
     CompiledElementwise,
     GeneratedElementwiseKernel,
@@ -9,7 +17,13 @@ from .verified_elementwise import (
 
 __all__ = [
     "CompiledElementwise",
+    "CompiledElementwiseTraining",
+    "GeneratedElementwiseBackwardKernel",
     "GeneratedElementwiseKernel",
+    "GeneratedElementwiseTraining",
     "compile_fx_elementwise",
+    "compile_fx_elementwise_training",
+    "load_generated_backward_kernel",
     "load_generated_kernel",
+    "load_generated_training",
 ]
